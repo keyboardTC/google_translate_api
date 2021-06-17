43d8b744fd86699767cb9422768ea82ff45d7ede
keyboardTC <37123270+keyboardTC@users.noreply.github.com>
2021-06-17 01:05:33 -0400

    updated the css link

--- js/script.js
+++ js/script.js
@@ -6,31 +6,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
     form_element.addEventListener(`submit`, (e) => {
         e.preventDefault();
+        let trans_msg = `empty`
         let text_msg = input_msg_element.value
         // const formData = new FormData(this);
         console.log(text_msg);
 
-    fetch("https://google-translate1.p.rapidapi.com/language/translate/v2", {
-        "method": "POST",
-        "headers": {
-            "content-type": "application/x-www-form-urlencoded",
-            "accept-encoding": "application/gzip",
-            "x-rapidapi-key": "c8ad9cbef7msh02ce88f81b3caebp1d6c5ejsna663c4baf122",
-            "x-rapidapi-host": "google-translate1.p.rapidapi.com"
-        },
-        "body": {
-            "q": "How are you ?",
-            "target": "fr",
-            "source": "en"
-        }
-    })
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    });
+        fetch("https://google-translate1.p.rapidapi.com/language/translate/v2/detect", {
+            "method": "POST",
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+                "accept-encoding": "application/gzip",
+                "x-rapidapi-key": "c8ad9cbef7msh02ce88f81b3caebp1d6c5ejsna663c4baf122",
+                "x-rapidapi-host": "google-translate1.p.rapidapi.com"
+            },
+            "body": {
+                "q": "quand vais-je commencer"
+            }
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 
+        console.log(trans_msg);
     })
 })
 
